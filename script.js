897df3daf538a17d2b2140d6d3c766d9ff5eaeb2
vsbluth <vbluth@saddleriverday.org>
2018-07-05 20:20:46 +0000

    add 2nd button

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const button1 = document.querySelector("#weather1");
+const button2 = document.querySelector("#weather2");
 const particles1 = document.querySelector("#particles1");
+const particles2 = document.querySelector("#particles2");
 
 button1.addEventListener('click', e => {
     console.log('add snow');
     particles1.setAttribute('particle-system', 'preset: snow; color: white');
+});
+
+button2.addEventListener('click', e => {
+    console.log('add spray');
+    // particle-system="color: #EF0000,#44CC00"
+    // Particle system uses 'default' preset, setting custom colors. -->
+   //  <<!--<a-entity position="0 3 0" particle-system="color: #EF0000,#44CC00"></a-entity>
+    particles2.setAttribute('particle-system', 'color: #EF0000,#44CC00');
 });
