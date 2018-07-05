5bc77bdd300b38fb625dc5bd28ba931633ccacb7
vsbluth <vbluth@saddleriverday.org>
2018-07-05 19:55:16 +0000

    add sphere

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const weather1 = document.querySelector("#weather1");
+const button1 = document.querySelector("#weather1");
 const particles1 = document.querySelector("#particles1");
 
-weather1.addEventListener('click', e => {
+button1.addEventListener('click', e => {
     console.log('add snow');
-    particles1.setAttribute('particle-system', 'preset: snow; color: red');
+    particles1.setAttribute('particle-system', 'preset: snow; color: white');
 });
