12f705cd6b5fd71feb9d9bb8b4df68d75d23ba82
Kelly Lougheed <kellylougheed@gmail.com>
2018-07-06 12:57:04 +0000

    Add two particle entities back

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const button1 = document.querySelector("#weather1");
 const button2 = document.querySelector("#weather2");
 const button3 = document.querySelector("#weather3");
 
-const particles = document.querySelector("#particles");
+const particles1 = document.querySelector("#particles1");
+const particles2 = document.querySelector("#particles2");
 
 const world = document.querySelector("#world");
 
 button1.addEventListener('click', e => {
     console.log('add snow');
-   particles.setAttribute('particle-system', 'preset: snow; color: green');
+   particles1.setAttribute('particle-system', 'preset: snow; color: green');
 });
 
 button2.addEventListener('click', e => {
@@ -16,7 +17,7 @@ button2.addEventListener('click', e => {
     // particle-system="color: #EF0000,#44CC00"
     // Particle system uses 'default' preset, setting custom colors. -->
    //  <<!--<a-entity position="0 3 0" particle-system="color: #EF0000,#44CC00"></a-entity>
-    particles.setAttribute('particle-system', 'color: #000000,#00CCFF');
+    particles2.setAttribute('particle-system', 'color: #000000,#00CCFF');
 });
 
 button3.addEventListener('click', e => {
